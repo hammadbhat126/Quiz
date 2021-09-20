c7231a2799c0290b49e61956a814a75688c0e034
Hammad <hammad@rpi.ae>
2021-09-20 14:44:49 +0400

    XML

--- app/src/main/java/com/kashsoft/quiz/QuizQuestionActivity.kt
+++ app/src/main/java/com/kashsoft/quiz/QuizQuestionActivity.kt
@@ -3,6 +3,7 @@ package com.kashsoft.quiz
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import kotlinx.android.synthetic.main.activity_quiz_question.*
 
 class QuizQuestionActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -10,5 +11,21 @@ class QuizQuestionActivity : AppCompatActivity() {
         setContentView(R.layout.activity_quiz_question)
         val  questionList = Constants.getQuestions()
         Log.i("Question Size" , "${questionList.size}")
+
+      val currentPostion = 1
+        val question: Question? = questionList[currentPostion -1]
+
+      progress_bar.progress = currentPostion
+        tv_progress.text = "$currentPostion" + "/" + progress_bar.max
+
+        tv_question.text = question!!.question
+        iv_image.setImageResource(question.image)
+        option_one.text= question.optionOne
+        option_two.text = question.optionTwo
+        option_three.text = question.optionThree
+        option_four.text= question.optionFour
+
+
+
     }
 }
